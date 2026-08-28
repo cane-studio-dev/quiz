f32979cba15375728f4e5160282fcf5a9b58de15
Marwan Dev <marwan282012@gmail.com>
2026-08-29 02:41:04 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'halaba-marefeya-v2';
+const CACHE_NAME = 'halaba-marefeya-v3';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
